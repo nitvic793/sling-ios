f60689e87a2582c2292b49fafe8aa5f3a60ed0ae
Satyam Krishna <satyamkrishna92@gmail.com>
2016-02-23 23:25:43 +0530

    Added Launch Screens

--- sling/View Controllers/LoginViewController.m
+++ sling/View Controllers/LoginViewController.m
@@ -16,22 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [[self view] setBackgroundColor:[CommonFunction getRandomColor]];
+    [[self view] setBackgroundColor:UIColorFromRGB(NAV_BAR_COLOR)];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(BOOL) prefersStatusBarHidden
+{
+    return YES;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
